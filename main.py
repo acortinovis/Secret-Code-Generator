3643037e13f8b2b712e5eb1731268fb693c3bc76
acortinovis <adelio.cortinovis@gmail.com>
2025-02-20 12:48:07 -0800

    finished program
    complete program, understanding difference between char and ord

--- main.py
+++ main.py
@@ -5,17 +5,22 @@
 # Define a function that takes a string and an integer shift value as parameters  
 def function(mes: str, shift: int):
 # Create an empty list to store the shifted characters 
-    list=[]
+    shift_message=[]
 # Loop through each character in the string:
-    for char in list:
-        if char.isalpha():
-            shift_car=chr(ord(char) + shift)
-#    If the character is a letter (A-Z or a-z):
-#        Shift the letter by adding the shift value to its ASCII code (use the ord function)
-#        Convert the new ASCII code back to a character (use the chr function)
-#        Add the shifted character to the list
+    for char in mes:
+        if char.isalpha(): #    If the character is a letter (A-Z or a-z):
+            shift_car=chr(ord(char) + shift)#        Shift the letter by adding the shift value to its ASCII code (use the ord function)
+                                            #        Convert the new ASCII code back to a character (use the chr function)
+            
+            shift_message.append(shift_car) #        Add the shifted character to the list
 #    If the character is not a letter:
-#        Add the character unchanged to the list
+        else:
+        #        Add the character unchanged to the list
+            shift_message.append(char)
 # After the loop, join the list into a string and return it  
+    return ''.join(shift_message)
 # Get user input for the message and shift value  
+
+user=function(input("enter the message "),int(input("enter shift value ")))
 # Call the function with the inputs and display the result
+print (user)
